b4b7749b9bebb91ab045ffb146cb5f0039ba2e02
arthishaxom <ashishpothal@gmail.com>
2025-05-17 21:08:33 +0530

    feat: add Gluestack UI provider and update app structure
    Introduce Gluestack UI provider to manage theme and color modes. Update app structure to include new screens and components, and configure Tailwind CSS with Gluestack plugin. This enhances the app's UI consistency and theming capabilities.

--- cvdekh/app/_layout.tsx
+++ cvdekh/app/_layout.tsx
@@ -2,5 +2,16 @@ import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name="index"
+      />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="dashboard" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
